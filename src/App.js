c506656c63945b3d326c689107f0e662a32a0e81
Andy Jacko <wisdom.mcr@gmail.com>
2022-09-27 14:22:23 +0100

    Added page context...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 
-function App() {
-  return <></>;
-}
+import PageContext from "./store/pageContext";
+import HomePage from "./pages/HomePage";
+import ServerPage from "./pages/ServerPage";
+
+const App = () => {
+  const pageCtx = useContext(PageContext);
+
+  return (
+    <>
+      {pageCtx.activePageType === 1 && <HomePage />}
+
+      {pageCtx.activePageType === 2 && <ServerPage />}
+    </>
+  );
+};
 
 export default App;
