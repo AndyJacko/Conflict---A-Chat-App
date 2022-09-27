59ce878e816ac396a0c26caa1f5f29e12214ada8
Andy Jacko <wisdom.mcr@gmail.com>
2022-09-27 14:33:18 +0100

    Created home page template...

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+import ServerList from "./page-sections/01 - ServerList/ServerList";
+import FriendChats from "./page-sections/03 - FriendChats/FriendChats";
+import ChatPanel from "./page-sections/04 - ChatPanel/ChatPanel";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="d-flex" id="page-container">
+      <div id="server-list">
+        <ServerList />
+      </div>
+
+      <div id="friend-chats">
+        <FriendChats />
+      </div>
+
+      <div id="chat-panel">
+        <ChatPanel />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
